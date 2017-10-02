f22a9caf4028c8e37579b16b0994f513b492de57
r1cebank <siyuangao@gmail.com>
2017-10-02 15:45:57 -0700

    Update project structure

---
+++ src/main/core/routes.py
@@ -0,0 +1,8 @@
+from ..main import app
+
+@app.route("/")
+def hello():
+    # This could also be returning an index.html
+    return '''Hello World from Flask in a uWSGI Nginx Docker container with \
+     Python 3.6 (from the example template), 
+     try also: <a href="/users/">/users/</a>'''
